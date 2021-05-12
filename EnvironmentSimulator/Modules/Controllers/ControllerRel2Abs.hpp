simsevi/esmini

Added Abs2Rel Controller #skip_bins

EnvironmentSimulator/Modules/Controllers/ControllerRel2Abs.hpp
@@ -1,0 +1,102 @@
+/* 
+ * esmini - Environment Simulator Minimalistic 
+ * https://github.com/esmini/esmini
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright (c) partners of Simulation Scenarios
+ * https://sites.google.com/view/simulationscenarios
+ */
+
+#pragma once
+
+#include <string>
+#include "Controller.hpp"
+#include "pugixml.hpp"
+#include "Parameters.hpp"
+#include "vehicle.hpp"
+//#include "OSCPrivateAction.hpp"
+#include "Entities.hpp"
+
+#define CONTROLLER_MY_TYPE_NAME "ControllerRel2Abs"
+
+namespace scenarioengine
+{
+	class ScenarioEngine;
+
+	// base class for controllers
+	class ControllerRel2Abs: public Controller
+	{
+	public:
+		struct PreSimData
+		{
+			std::vector<double> time;
+			std::vector<double> speeds;
+			std::vector<double> posX;
+			std::vector<double> posY;
+		};
+
+		struct position_copy
+		{
+			Object* object;
+			roadmanager::Position* pos;
+			double speed;
+			int dirtyBits;
+		};
+
+		enum ctrl_mode {
+			RELATIVE,
+			ABSOLUTE
+		};
+
+		std::vector<PreSimData> data_vector;
+		PreSimData actualData;
+		PreSimData data;
+		double timestamp;
+
+		// Presim parameters
+		double pred_horizon = 1; // 1 second 
+		double pred_timestep = 0.1; // (milliseconds) Midpoint between min and max, maybe change later
+		double pred_nbr_timesteps = pred_horizon / pred_timestep;
+		int action_whitelist[7] = { 0, 1, 2, 3, 4, 11, 12 };
+		double switching_threshold = 1.8; //1.5 m 
+
+		//CSV loggning
+		std::ofstream logData;
+		int csv_iter;
+
+		ControllerRel2Abs(InitArgs* args);
+
+		void Init();
+		void Step(double timeStep);
+		void Activate(int domainMask);
+		void ReportKeyEvent(int key, bool down);
+		void SetScenarioEngine(ScenarioEngine* scenarioEngine) { scenarioEngine_ = scenarioEngine; };
+		void CopyPosition(Object* object, position_copy* obj_copy);
+
+		static const char* GetTypeNameStatic() { return CONTROLLER_MY_TYPE_NAME; }
+		virtual const char* GetTypeName() { return GetTypeNameStatic(); }
+		static const int GetTypeStatic() { return Controller::Type::CONTROLLER_TYPE_MY; }
+		virtual int GetType() { return GetTypeStatic(); }
+
+	private:
+		int ego_obj;
+		double prev_ego_speed;
+		double prev_target_speed;
+		bool switchNow;
+		//vector with pairs of timestep and speed before that timestep
+		std::vector<std::pair<double, double>> speeds;
+
+		//alters ego_obj to correct entities index for ego
+		void findEgo();
+
+#ifdef _SCENARIO_VIEWER
+		viewer::CarModel* gfx_model;
+#endif
+		ScenarioEngine* scenarioEngine_;
+	};
+
+	Controller* InstantiateControllerRel2Abs(void* args);
+}

EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/ScenarioEngine.cpp
@@ -14,6 +14,7 @@
 #include "CommonMini.hpp"
 #include "ControllerFollowGhost.hpp"
 #include "ControllerExternal.hpp"
+#include "ControllerRel2Abs.hpp"
 
 #define WHEEL_RADIUS 0.35
 #define STAND_STILL_THRESHOLD 1e-3  // meter per second
@@ -187,6 +188,11 @@ void ScenarioEngine::step(double deltaSimTime)
 	{
 		if (init.private_action_[i]->IsActive())
 		{
+			//Add action to object initActions vector if it doesn't contain the action
+			if (std::find(init.private_action_[i]->object_->initActions_.begin(), init.private_action_[i]->object_->initActions_.end(), init.private_action_[i]) == init.private_action_[i]->object_->initActions_.end())
+			{
+				init.private_action_[i]->object_->initActions_.push_back(init.private_action_[i]);
+			}
 			//LOG("Stepping action of type %d", init.private_action_[i]->action_[j]->type_)
 			init.private_action_[i]->Step(getSimulationTime(), deltaSimTime);
 			init.private_action_[i]->UpdateState();
@@ -269,6 +275,25 @@ void ScenarioEngine::step(double deltaSimTime)
 						{
 							Event *event = maneuver->event_[m];
 
+							//add event to objectEvents vector
+							if (event->IsTriggable() || event->IsActive())
+							{
+								for (size_t n = 0; n < event->action_.size(); n++)
+								{
+									OSCAction* action = event->action_[n];
+									if (action->base_type_ == OSCAction::BaseType::PRIVATE)
+									{
+										OSCPrivateAction* pa = (OSCPrivateAction*)action;
+										if (!pa->object_->containsEvent(event))
+										{
+											pa->object_->addEvent(event);
+											break;
+										}
+											
+									}
+								}								
+							}
+
 							if (event->IsTriggable())
 							{
 								// Check event conditions
@@ -289,6 +314,18 @@ void ScenarioEngine::step(double deltaSimTime)
 											{
 												if (maneuver->event_[n]->IsActive())
 												{
+													//remove event from objectEvents vector
+													for (size_t o = 0; o < maneuver->event_[n]->action_.size(); o++)
+													{
+														OSCAction* action = maneuver->event_[n]->action_[o];
+														if (action->base_type_ == OSCAction::BaseType::PRIVATE)
+														{
+															OSCPrivateAction* pa = (OSCPrivateAction*)action;
+															pa->object_->removeEvent(event);
+															break;
+														}
+													}
+
 													maneuver->event_[n]->End();
 													LOG("Event %s ended, overwritten by event %s",
 														maneuver->event_[n]->name_.c_str(), event->name_.c_str());
@@ -306,6 +343,7 @@ void ScenarioEngine::step(double deltaSimTime)
 										}
 										else
 										{
+
 											event->Start(simulationTime_, deltaSimTime);
 										}
 									}
@@ -320,6 +358,8 @@ void ScenarioEngine::step(double deltaSimTime)
 										{
 											LOG("Event(s) ongoing, %s will run in parallel", event->name_.c_str());
 										}
+
+										
 										event->Start(simulationTime_, deltaSimTime);
 									}
 									else
@@ -345,6 +385,18 @@ void ScenarioEngine::step(double deltaSimTime)
 								}
 								if (!active)
 								{
+									//remove event from objectEvents vector
+									for (size_t n = 0; n < event->action_.size(); n++)
+									{
+										OSCAction* action = event->action_[n];
+										if (action->base_type_ == OSCAction::BaseType::PRIVATE)
+										{
+											OSCPrivateAction* pa = (OSCPrivateAction*)action;
+											pa->object_->removeEvent(event);
+											break;
+										}
+									}
+
 									// Actions done -> Set event done
 									event->End();
 								}
@@ -525,6 +577,10 @@ void ScenarioEngine::parseScenario()
 		for (size_t i = 0; i < scenarioReader->controller_.size(); i++)
 		{
 			scenarioReader->controller_[i]->Init();
+			if (scenarioReader->controller_[i]->GetType() == Controller::Type::CONTROLLER_TYPE_MY)
+			{
+				((ControllerRel2Abs*)(scenarioReader->controller_[i]))->SetScenarioEngine(this);
+			}
 		}
 
 		// find out maximum headstart time for ghosts

eknabe/esmini_fork

EnvironmentSimulator/Modules/Controllers/ControllerRel2Abs.hpp
@@ -1,0 +1,100 @@
+/* 
+ * esmini - Environment Simulator Minimalistic 
+ * https://github.com/esmini/esmini
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright (c) partners of Simulation Scenarios
+ * https://sites.google.com/view/simulationscenarios
+ */
+
+#pragma once
+
+#include <string>
+#include "Controller.hpp"
+#include "pugixml.hpp"
+#include "Parameters.hpp"
+#include "Entities.hpp"
+
+#define CONTROLLER_REL2ABS_TYPE_NAME "ControllerRel2Abs"
+
+namespace scenarioengine
+{
+	class ScenarioEngine;
+
+	// base class for controllers
+	class ControllerRel2Abs: public Controller
+	{
+	public:
+		struct PreSimData
+		{
+			std::vector<double> time;
+			std::vector<double> speeds;
+			std::vector<double> posX;
+			std::vector<double> posY;
+		};
+
+		struct position_copy
+		{
+			Object* object;
+			roadmanager::Position* pos;
+			double speed;
+			int dirtyBits;
+		};
+
+		enum ctrl_mode {
+			RELATIVE,
+			ABSOLUTE
+		};
+
+		std::vector<PreSimData> data_vector;
+		PreSimData actualData;
+		PreSimData data;
+		double timestamp;
+
+		// Presim parameters
+		double pred_horizon = 1; // 1 second 
+		double pred_timestep = 0.1; // (milliseconds) Midpoint between min and max, maybe change later
+		double pred_nbr_timesteps = pred_horizon / pred_timestep;
+		int action_whitelist[7] = { 0, 1, 2, 3, 4, 11, 12 };
+		double switching_threshold = 1.8; //1.5 m 
+
+		//CSV loggning
+		std::ofstream logData;
+		int csv_iter;
+
+		ControllerRel2Abs(InitArgs* args);
+
+		void Init();
+		void Step(double timeStep);
+		void Activate(int domainMask);
+		void ReportKeyEvent(int key, bool down);
+		void SetScenarioEngine(ScenarioEngine* scenarioEngine) { scenarioEngine_ = scenarioEngine; };
+		void CopyPosition(Object* object, position_copy* obj_copy);
+
+		static const char* GetTypeNameStatic() { return CONTROLLER_REL2ABS_TYPE_NAME; }
+		virtual const char* GetTypeName() { return GetTypeNameStatic(); }
+		static const int GetTypeStatic() { return Controller::Type::CONTROLLER_TYPE_REL2ABS; }
+		virtual int GetType() { return GetTypeStatic(); }
+
+	private:
+		int ego_obj;
+		double prev_ego_speed;
+		double prev_target_speed;
+		bool switchNow;
+		//vector with pairs of timestep and speed before that timestep
+		std::vector<std::pair<double, double>> speeds;
+
+		//alters ego_obj to correct entities index for ego
+		void findEgo();
+
+#ifdef _SCENARIO_VIEWER
+		viewer::CarModel* gfx_model;
+#endif
+		ScenarioEngine* scenarioEngine_;
+	};
+
+	Controller* InstantiateControllerRel2Abs(void* args);
+}

EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/ScenarioEngine.cpp
@@ -14,6 +14,7 @@
 #include "CommonMini.hpp"
 #include "ControllerFollowGhost.hpp"
 #include "ControllerExternal.hpp"
+#include "ControllerRel2Abs.hpp"
 
 #define WHEEL_RADIUS 0.35
 #define STAND_STILL_THRESHOLD 1e-3  // meter per second
@@ -187,6 +188,11 @@ void ScenarioEngine::step(double deltaSimTime)
 	{
 		if (init.private_action_[i]->IsActive())
 		{
+			//Add action to object initActions vector if it doesn't contain the action
+			if (std::find(init.private_action_[i]->object_->initActions_.begin(), init.private_action_[i]->object_->initActions_.end(), init.private_action_[i]) == init.private_action_[i]->object_->initActions_.end())
+			{
+				init.private_action_[i]->object_->initActions_.push_back(init.private_action_[i]);
+			}
 			//LOG("Stepping action of type %d", init.private_action_[i]->action_[j]->type_)
 			init.private_action_[i]->Step(getSimulationTime(), deltaSimTime);
 			init.private_action_[i]->UpdateState();
@@ -269,6 +275,25 @@ void ScenarioEngine::step(double deltaSimTime)
 						{
 							Event *event = maneuver->event_[m];
 
+							//add event to objectEvents vector
+							if (event->IsTriggable() || event->IsActive())
+							{
+								for (size_t n = 0; n < event->action_.size(); n++)
+								{
+									OSCAction* action = event->action_[n];
+									if (action->base_type_ == OSCAction::BaseType::PRIVATE)
+									{
+										OSCPrivateAction* pa = (OSCPrivateAction*)action;
+										if (!pa->object_->containsEvent(event))
+										{
+											pa->object_->addEvent(event);
+											break;
+										}
+											
+									}
+								}								
+							}
+
 							if (event->IsTriggable())
 							{
 								// Check event conditions
@@ -289,6 +314,18 @@ void ScenarioEngine::step(double deltaSimTime)
 											{
 												if (maneuver->event_[n]->IsActive())
 												{
+													//remove event from objectEvents vector
+													for (size_t o = 0; o < maneuver->event_[n]->action_.size(); o++)
+													{
+														OSCAction* action = maneuver->event_[n]->action_[o];
+														if (action->base_type_ == OSCAction::BaseType::PRIVATE)
+														{
+															OSCPrivateAction* pa = (OSCPrivateAction*)action;
+															pa->object_->removeEvent(event);
+															break;
+														}
+													}
+
 													maneuver->event_[n]->End();
 													LOG("Event %s ended, overwritten by event %s",
 														maneuver->event_[n]->name_.c_str(), event->name_.c_str());
@@ -306,6 +343,7 @@ void ScenarioEngine::step(double deltaSimTime)
 										}
 										else
 										{
+
 											event->Start(simulationTime_, deltaSimTime);
 										}
 									}
@@ -320,6 +358,8 @@ void ScenarioEngine::step(double deltaSimTime)
 										{
 											LOG("Event(s) ongoing, %s will run in parallel", event->name_.c_str());
 										}
+
+										
 										event->Start(simulationTime_, deltaSimTime);
 									}
 									else
@@ -345,6 +385,18 @@ void ScenarioEngine::step(double deltaSimTime)
 								}
 								if (!active)
 								{
+									//remove event from objectEvents vector
+									for (size_t n = 0; n < event->action_.size(); n++)
+									{
+										OSCAction* action = event->action_[n];
+										if (action->base_type_ == OSCAction::BaseType::PRIVATE)
+										{
+											OSCPrivateAction* pa = (OSCPrivateAction*)action;
+											pa->object_->removeEvent(event);
+											break;
+										}
+									}
+
 									// Actions done -> Set event done
 									event->End();
 								}
@@ -525,6 +577,10 @@ void ScenarioEngine::parseScenario()
 		for (size_t i = 0; i < scenarioReader->controller_.size(); i++)
 		{
 			scenarioReader->controller_[i]->Init();
+			if (scenarioReader->controller_[i]->GetType() == Controller::Type::CONTROLLER_TYPE_REL2ABS)
+			{
+				((ControllerRel2Abs*)(scenarioReader->controller_[i]))->SetScenarioEngine(this);
+			}
 		}
 
 		// find out maximum headstart time for ghosts